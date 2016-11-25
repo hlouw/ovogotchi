87050d241a4b7cbc2bbfe24ae7bcd19f7dc73ee5
Fred Parke <fjcparke@gmail.com>
2016-11-25 11:55:33 +0000

    reset default state

--- client/src/app/emotion/emotion.reducer.ts
+++ client/src/app/emotion/emotion.reducer.ts
@@ -18,7 +18,7 @@ export enum EmotionalState {
 export const STATE_UPDATE = 'STATE_UPDATE';
 
 const INITIAL_STATE: OverallState = {
-    state: EmotionalState.ANGRY,
+    state: EmotionalState.NEUTRAL,
     wellbeing: 70
 };
 
